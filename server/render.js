4d5eec3abfe58337bfd18a8b23ecb9141bd8bff5
KadoBOT <ricardo.ambrogi@gmail.com>
2017-06-14 10:41:00 +0200

    error

--- server/render.js
+++ server/render.js
@@ -39,35 +39,35 @@ export default ({ clientStats, outputPath }) => (req, res, next) => {
   console.log('SERVED SCRIPTS', scripts)
   console.log('SERVED STYLESHEETS', stylesheets)
 
-  res.send(
-    `<!doctype html>
-      <html>
-        <head>
-          <meta charset="utf-8">
-          <title>react-universal-component-boilerplate</title>
-          ${styles}
-        </head>
-        <body>
-          <div id="root">${app}</div>
-          ${js}
-        </body>
-      </html>`
-  )
+  // res.send(
+  //   `<!doctype html>
+  //     <html>
+  //       <head>
+  //         <meta charset="utf-8">
+  //         <title>react-universal-component-boilerplate</title>
+  //         ${styles}
+  //       </head>
+  //       <body>
+  //         <div id="root">${app}</div>
+  //         ${js}
+  //       </body>
+  //     </html>`
+  // )
 
   // COMMENT the above `res.send` call
   // and UNCOMMENT below to test rendering React components:
 
-  // const html = ReactDOM.renderToStaticMarkup(
-  //   <html>
-  //     <head>
-  //       <Css />
-  //     </head>
-  //     <body>
-  //       <div id="root" dangerouslySetInnerHTML={{ __html: app }} />
-  //       <Js />
-  //     </body>
-  //   </html>
-  // )
+  const html = ReactDOM.renderToStaticMarkup(
+    <html>
+      <head>
+        <Css />
+      </head>
+      <body>
+        <div id="root" dangerouslySetInnerHTML={{ __html: app }} />
+        <Js />
+      </body>
+    </html>
+  )
 
-  // res.send(`<!DOCTYPE html>${html}`)
+  res.send(`<!DOCTYPE html>${html}`)
 }
