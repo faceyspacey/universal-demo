2325bb1b128f726b4ab0d3794b71f9559fabe9e3
Sebastian Werner <s.werner@sebastian-software.de>
2017-08-13 12:59:36 +0200

    Updated code

--- src/loadMessages.js
+++ src/loadMessages.js
@@ -1,8 +1,11 @@
 export default function loadMessages(lang) {
+  const wrapped = import(`./messages/${lang}`)
+
   if (typeof window !== 'undefined') {
-    return import(`./messages/${lang}`).then(msgs => msgs.default)
+    return wrapped.then(msgs => msgs.default)
   }
 
   /* eslint-disable global-require, import/no-dynamic-require, prefer-template */
-  return Promise.resolve(require('./messages/' + lang).default)
+  // return Promise.resolve(require('./messages/' + lang).default)
+  return __webpack_require__(wrapped.resolve()).default
 }
