60f15e9c25d8a2d4d39658e8fcc209ec38b5a525
Eric Ogden <parties@users.noreply.github.com>
2019-03-02 21:20:55 -0800

    fix(UniversalComponent): fix page import (#43)
    fixed the determineHowToLoad function's broken dynamic import
    
    fix #42

--- src/UniversalComponent.js
+++ src/UniversalComponent.js
@@ -7,7 +7,7 @@ import NotFound from './components/NotFound'
 // small function that allows page to be a string or a dynamic import function
 // <UniversalComponent page={()=>{import('../../someFolder/Component.js')}}
 // Its great for complex folder structures. You can leverage code completion
-const determineHowToLoad = ({ page }) => typeof page !== 'string' ? () => page() : () => import(`./components/${page}`)
+const determineHowToLoad = ({ page }) => typeof page !== 'string' ? () => page() : import(`./${page}`)
 
 const UniversalComponent = universal(determineHowToLoad, {
   onError: error => {
